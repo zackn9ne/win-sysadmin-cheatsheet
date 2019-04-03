9c477f55df5ad24943ef9434df3441ccf4e27147
zackn9ne <zackn9ne@gmail.com>
2019-04-03 11:33:24 -0400

    Create install_slack.ps1

---
+++ install_slack.ps1
@@ -0,0 +1,16 @@
+$soft_name = "slack"
+$link = "http://slack.com/ssb/download-win64-msi"
+$file = "c:\Windows\Temp\slack.msi"
+
+Invoke-WebRequest $link -OutFile $file
+
+
+Function Install_MSI_slack_Installer{
+$arguments= ' /qn /l*v .\install_slack.txt' 
+Start-Process `
+     -file  $file `
+     -arg $arguments `
+     -passthru | wait-process
+}
+
+Install_MSI_slack_Installer 
